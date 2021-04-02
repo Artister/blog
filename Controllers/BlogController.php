919d4ec21edb358c51afd1a23044a81ebda85b6c
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-04-01 21:52:44 -0400

    update BlogController

--- Controllers/BlogController.php
+++ Controllers/BlogController.php
@@ -2,11 +2,19 @@
 
 namespace Application\Controllers;
 
+use Artister\Entity\EntityContext;
 use Artister\Web\Mvc\Controller;
 use Artister\Web\Mvc\IActionResult;
 
 class BlogController extends Controller
 {
+    private EntityContext $DbManager;
+
+    public function __construct(EntityContext $dbManager)
+    {
+        $this->DbManager = $dbManager;
+    }
+
     public function index() : IActionResult
     {
         return $this->view();
