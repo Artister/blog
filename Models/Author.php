c9c806fba68dd32330dcef931eb0fa4195a67dbe
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-04-28 21:34:37 -0400

    updare Author model

--- Models/Author.php
+++ Models/Author.php
@@ -10,6 +10,14 @@ class Author implements IEntity
     private int $Id;
     private int $UserId;
     private string $Name;
+    private string $Gender;
+    private string $Occupation;
+    private string $Location;
+    private string $Picture;
+    private string $Description;
+    private string $Email;
+    private string $Phone;
+    private string $Link;
 
     // navigation properties
     private User $User;
