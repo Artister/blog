b6642a051177878bbe6ec5ba2dfe857f935621ed
Mahfoud <mahfoud@mahfoud-HP.local>
2021-06-13 15:42:03 +0100

    Create a seeder controller

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -14,54 +14,48 @@ class HomeController extends Controller
         $this->DbManager = $dbManager;
     }
 
-    public function index() : IActionResult
+    public function index(): IActionResult
     {
         return $this->view();
     }
 
-    public function about() : IActionResult
+    public function about(): IActionResult
     {
         return $this->view();
     }
 
-    public function contact(ContactForm $form) : IActionResult
+    public function contact(ContactForm $form): IActionResult
     {
-        if (!$form->isValide())
-        {
+        if (!$form->isValide()) {
             return $this->view();
         }
 
-        $headers = "From: noreply@yourdomain.com\n";
+        $headers = "From: mahfoud_bousba@yahoo.com\n";
         $headers .= "Reply-To: {$form->Email}";
-        $to = 'yourname@yourdomain.com';
+        $to = 'mahfoudbousba2@gmail.com';
         $subject = "Website Contact Form:  {$form->Subject}";
         $message = "You have received a new message from your website contact form\n";
         $message .= "Here are the details:\n";
         $message .= "Name: {$form->Name}\n";
         $message .= "Email: {$form->Email}\n";
         $message .= "Message: {$form->Message}";
-   
+
         $success = mail($to, $subject, $message, $headers);
 
-        if ($success)
-        {
+        if ($success) {
             $this->ViewData['response'] = "your message has been sent successfully";
-        }
-        else
-        {
+        } else {
             $this->ViewData['response'] = "Sorry your message wasn't delivered!";
         }
 
         return $this->content("You message has been sent.");
     }
 
-    public function error() : IActionResult
+    public function error(): IActionResult
     {
         $error = new \Exception("Sorry! Looks like this page doesn't exist.", 404);
-        if($this->HttpContext->Error)
-        {
-            switch ($this->HttpContext->Error->getCode())
-            {
+        if ($this->HttpContext->Error) {
+            switch ($this->HttpContext->Error->getCode()) {
                 case 404:
                     break;
                 case 403:
---
+++ Controllers/SeederController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Application\Controllers;
+
+use DevNet\Entity\EntityContext;
+use DevNet\Web\Mvc\Controller;
+use DevNet\Web\Mvc\IActionResult;
+
+class SeederController extends Controller
+{
+    public function __construct(EntityContext $dbManager)
+    {
+        $this->DbManager = $dbManager;
+    }
+
+    public function index(): IActionResult
+    {
+        return $this->view();
+    }
+
+    public function post(): IActionResult
+    {
+        return $this->view();
+    }
+}
