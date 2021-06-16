fc35ee750b810273dca7d79215476c701067ab2d
Mahfoud <mahfoud@mahfoud-HP.local>
2021-06-16 12:29:18 +0100

    Automatic profile creation in Register method

--- Controllers/User/AccountController.php
+++ Controllers/User/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace Application\Controllers\User;
 
+use Application\Models\Author;
 use DevNet\Web\Mvc\Controller;
 use DevNet\Web\Mvc\IActionResult;
 use DevNet\Web\Mvc\Filters\AuthorizeFilter;
@@ -11,40 +12,43 @@ use DevNet\Web\Identity\UserManager;
 use Application\Models\LoginForm;
 use Application\Models\RegisterForm;
 use Application\Models\User;
+use DevNet\System\Linq;
+use DevNet\Entity\EntityContext;
 
 class AccountController extends Controller
 {
     private IdentityManager $Identity;
     private UserManager $Users;
+    private EntityContext $DbManager;
 
-    public function __construct(IdentityManager $identity, UserManager $users)
+    public function __construct(IdentityManager $identity, UserManager $users, EntityContext $dbManager)
     {
         $this->Identity = $identity;
         $this->Users = $users;
+        $this->DbManager = $dbManager;
 
         $this->filter('index', AuthorizeFilter::class);
         $this->filter('login', AntiForgeryFilter::class);
         $this->filter('register', AntiForgeryFilter::class);
     }
 
-    public function index() : IActionResult
+    public function index(): IActionResult
     {
         $user = $this->Users->getUser();
         $this->ViewData['Email'] = $user->Username;
         return $this->view();
     }
 
-    public function login(LoginForm $form) : IActionResult
+    public function login(LoginForm $form): IActionResult
     {
         $user = $this->HttpContext->User;
 
-        if ($user->isAuthenticated())
-        {
+        if ($user->isAuthenticated()) {
+            $this->ViewData['author'] = $this->DbManager->Authors->find($user->Id);
             return $this->redirect('/user/account');
         }
 
-        if (!$form->isValide())
-        {
+        if (!$form->isValide()) {
             return $this->view();
         }
 
@@ -53,16 +57,15 @@ class AccountController extends Controller
         return $this->redirect('/user/account');
     }
 
-    public function logout() : IActionResult
+    public function logout(): IActionResult
     {
         $this->Identity->signOut();
         return $this->redirect('/user/account/login');
     }
 
-    public function register(RegisterForm $form) : IActionResult
+    public function register(RegisterForm $form): IActionResult
     {
-        if (!$form->isValide())
-        {
+        if (!$form->isValide()) {
             return $this->view();
         }
 
@@ -71,6 +74,23 @@ class AccountController extends Controller
         $user->Password = $form->Password;
         $this->Users->create($user);
 
-        return $this->content("User {$user->Username} has been created");
+        // find the last user
+        $last = $this->DbManager->Users->last();
+        // profile creation
+        $author = new Author();
+        $author->UserId = $last->Id;
+        $author->Name = $form->Name;
+        $author->Gender = "My Gender";
+        $author->Occupation = "My Occupation";
+        $author->Location = "My Loction";
+        $author->Description = "My Description";
+        $author->Phone = "9999999999";
+        $author->Email =  $form->Email;
+        $author->Link = "My link";
+        $author->Photo = "avatar1.png";
+        $this->DbManager->Authors->add($author);
+        $this->DbManager->save();
+
+        return $this->content("User {$user->Username} and its profile has been created.");
     }
 }
